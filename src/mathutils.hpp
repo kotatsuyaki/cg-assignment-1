a96b695fe6b502ec6d69940388091c179c4590e3
kotatsuyaki <robinhuang123@gmail.com>
2022-04-24 21:36:16 +0800

    Fix truncation warnings

--- src/mathutils.hpp
+++ src/mathutils.hpp
@@ -1,7 +1,7 @@
 #ifndef MATHUTILS_HPP_
 #define MATHUTILS_HPP_
 
-constexpr float PI = 3.14159265358979323846;
+constexpr float PI = 3.14159265358979323846f;
 constexpr float deg2rad(float deg) { return deg * PI / 180.0f; }
 
 #endif
